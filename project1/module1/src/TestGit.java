e4a90d976491c626902477dac754d28a1632d35f
zkg1024 <zhao1506412996@163.com>
2018-12-22 19:00:59 +0800

    添加module1到git

--- project1/module1/src/TestGit.java
+++ project1/module1/src/TestGit.java
@@ -1,6 +1,5 @@
 public class TestGit {
     public static void main(String[] args) {
         System.out.println(11);
-        System.out.println(11);
     }
 }
---
+++ project1/module1/src/TestGit1.java
@@ -0,0 +1,7 @@
+public class TestGit1 {
+    public static void main(String[] args) {
+        System.out.println(11);
+        System.out.println(11);
+        System.out.println(11);
+    }
+}
