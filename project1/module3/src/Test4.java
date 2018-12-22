86f80639b60432719f16188405a5e808a1769555
zkg1024 <zhao1506412996@163.com>
2018-12-22 19:53:32 +0800

    test3

--- project1/module3/src/Test4.java
+++ project1/module3/src/Test4.java
@@ -2,5 +2,6 @@ public class Test4 {
     public static void main(String[] args) {
         System.out.println("asas");
         System.out.println("asas");
+        System.out.println("asas");
     }
 }
